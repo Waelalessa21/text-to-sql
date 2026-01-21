4ffb218dbdc9573024ab2d634b1a4a68b564be8e
khaldsh <kh.learn4@gmail.com>
2026-01-21 17:12:49 +0300

    add system prompt to generate sql queries based on user requests.

--- text_to_sql_engine/src/ai_engine/model/prompt.py
+++ text_to_sql_engine/src/ai_engine/model/prompt.py
@@ -1,1 +1,14 @@
 # defines the prompt that will be used to generate the SQL query.
+
+SQL_PROMPT_TEMPLATE = """<|im_start|>system
+You are a SQL expert. Given the database schema below, write a valid SQL query to answer the user's question.
+Return ONLY the SQL code. Do not include markdown formatting or explanations.
+
+Schema:
+{schema}
+<|im_end|>
+<|im_start|>user
+Question: {question}
+<|im_end|>
+<|im_start|>assistant
+"""
