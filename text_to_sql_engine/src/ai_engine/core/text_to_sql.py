5c8c7d73b943d3d95b8efa73965f7130032ed738
khaldsh <kh.learn4@gmail.com>
2026-01-26 02:03:28 +0300

    Integrates the LLM logic with the database manager to handle the text-to-SQL pipeline, including prompt construction, model inference, and output cleaning.

--- text_to_sql_engine/src/ai_engine/core/text_to_sql.py
+++ text_to_sql_engine/src/ai_engine/core/text_to_sql.py
@@ -1,1 +1,29 @@
 # The process of converting a natural language question into a valid SQL query that can be executed on a database.
+
+from ..model.loader import load_ai_model
+from ..model.prompt import SQL_PROMPT_TEMPLATE
+from ..core.database_manager import DatabaseManager
+import re
+
+
+class TextToSQLEngine:
+    def __init__(self, db_manager: str):
+        self.db_manager = db_manager
+        self.llm = load_ai_model()
+
+    def process_query(self, user_text: str):
+        schema = self.db_manager.get_schema_text() 
+        
+        full_prompt = SQL_PROMPT_TEMPLATE.format(
+            schema=schema, 
+            question=user_text
+        )
+        response = self.llm(full_prompt, max_tokens=250, stop=["<|im_end|>"])
+        
+        raw_sql = response["choices"][0]["text"].strip()
+        # clean the respone
+        clean_sql = re.sub(r"```sql|```", "", raw_sql).strip()
+        
+        # 3. execute SQL
+        cols, rows = self.db_manager.execute_query(clean_sql)
+        return clean_sql, cols, rows
